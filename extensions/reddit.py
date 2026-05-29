7a00842c5a7b21fad0b9b046545da12cd41156dd
EthanC <16727756+EthanC@users.noreply.github.com>
2026-05-29 03:07:55 -0500

    Feat(Reddit): Add r/ModernWarfare4 to communities

--- extensions/reddit.py
+++ extensions/reddit.py
@@ -22,6 +22,7 @@ group: SlashGroup[GatewayClient] = plugin.include_slash_group(
     "reddit", "Commands to manage Reddit communities."
 )
 communities: dict[str, str] = {
+    "r/ModernWarfare4": "ModernWarfare4",
     "r/CODBlackOps7": "CODBlackOps7",
     "r/BlackOps6": "BlackOps6",
     "r/ModernWarfareIII": "ModernWarfareIII",
